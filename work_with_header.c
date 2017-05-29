17d618a0601b060059a5f278da542003fcc1184a
SifuUA <alekseykres@gmail.com>
2017-05-29 16:26:47 +0300

    parse header

--- work_with_header.c
+++ work_with_header.c
@@ -4,8 +4,27 @@
 
 #include "op.h"
 
-void 	header_parse(t_asm *asemb, int fd)
+unsigned int	do_big_endian(unsigned int magic, int size)
 {
+	unsigned int	res;
+	int 			i;
 
+	i = 0;
+	res = 0;
+	size = (size - 1) * 8;
+	while (i <= size)
+	{
+		res |= ((magic >> (size - i) & 0xFF)) << i;
+		i += 8;
+	}
+	return (res);
 }
 
+void			header_parse(t_asm *head, int fd)
+{
+	head->header->magic = do_big_endian(COREWAR_EXEC_MAGIC, 4);
+	ft_bzero(head->header->prog_name, PROG_NAME_LENGTH + 1);
+	ft_bzero(head->header->comment, COMMENT_LENGTH + 1);
+	fill_header_and_get_size();
+
+}
