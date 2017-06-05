48626a2f7f5cdf66dc991a8029d8c4c78cbf30d9
SifuUa <alekseykres@gmail.com>
2017-06-05 14:48:59 +0300

    Merge branch 'master' of https://github.com/coreWWll/corewar
    # Conflicts:
    #	lists_work.c
    #	virtual_mashine/libft/ft_printf/ft_lentoc.c

--- asm/work_with_commands.c
+++ asm/work_with_commands.c
@@ -6,38 +6,6 @@
 int	size[] = {4, 4, 0, 0, 0, 4, 4, 4, 2, 2, 2, 2, 4, 2, 2, 4};
 int codage_octal[] = {0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1};
 
-/*int 			if_lable(int *ar)
-{
-	int i;
-
-	i = 0;
-	while (i < 3)
-	{
-		if (ar[i] > 0)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-int 			get_index(int *ar)
-{
-	int i;
-	int j;
-
-	i = 0;
-	j = 0;
-	while (i < 3)
-	{
-		if (ar[i] == 1)
-			j = i;
-		i++;
-	}
-	return (j);
-}
-*/
-
-
 void	write_codage_octal(t_asm *begin, int fd)
 {
 	int i;
@@ -100,10 +68,6 @@ int 			find_lable(t_asm *begin, char *lable, int val_cur)
 		}
 		node = node->next;
 	}
-	/*if (node && (node->l_flag[0] == 1 || node->l_flag[1] == 1 ||
-		node->l_flag[2] == 1))
-		res += find_lable(begin, begin->args[get_index(begin->l_flag)]); // проблема с записью лейбы, Антон фикс
-	*/
 	 return (res);
 }
 
