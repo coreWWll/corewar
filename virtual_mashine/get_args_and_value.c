a7d89c6bed7c5412da6ddf7342e8abf8a31d28fb
dburtnja <dostup.te@gmail.com>
2017-06-09 17:41:01 +0300

    make get_args_and_value

--- virtual_mashine/get_args_and_value.c
+++ virtual_mashine/get_args_and_value.c
@@ -15,32 +15,67 @@ void	read_args_from_char(t_car *car, unsigned char c, int pos)
 	car->args[2].name = (buf = (c >> 2) << 6) >> 6;
 	while (i < 3)
 	{
+		if (car->args[i].name == 1)
+			car->args[i].name = T_REG;
+		if (car->args[i].name == 2)
+			car->args[i].name = T_DIR;
 		if (car->args[i].name == 3)
-			car->args[i].name = 4;
+			car->args[i].name = T_IND;
 		i++;
 	}
 }
 
-int 	compare_args(int *example, int *args)
+int		read_one_arg(t_car *car, unsigned char *map, int read_size)
 {
-	int buf;
+	int		nbr;
+	int		i;
 
-	ft_printf("\nexample = %08b\n", example[0]);
-	ft_printf("args = %08b\n", args[0]);
+	i = 0;
+	nbr = 0;
+	while (read_size > i)
+	{
+		nbr = nbr << 8;
+		nbr |= map[i];
+		i++;
+	}
+	return (nbr);
+}
 
-	buf = example[0] & args[0];
-	ft_printf("& = %08b\n", buf);
-	return TRUE;
+int 	read_args(t_car *car, unsigned char *map)
+{
+	int		i;
+	int		error;
+	int		buf;
+	int		label_size;
+	int 	read_size;
+
+	i = 0;
+	error = FALSE;
+	label_size = car->op_tabble.codage_octal == 0 ? 4 : 2;
+	while (i < car->op_tabble.args_am)
+	{
+		read_size = car->args[i].name == 4 ? label_size : car->args[i].name;
+		buf = car->op_tabble.args[i] & car->args[i].name;
+		if (buf != car->args[i].name || buf == 0)
+			error = TRUE;
+		if (error == FALSE)
+			car->args[i].value = read_one_arg(car, map, read_size);
+		car->arg_size += read_size;
+		map += read_size;
+		i++;
+	}
+	return (error);
 }
 
 int		get_args_nd_value(t_car *car, t_vm *main_struct)
 {
 	int		local_pos;
-	int		label_size;
 
-	label_size = car->op_tabble.codage_octal == 0 ? 4 : 2;
 	local_pos = car->pos + 1;
-	read_args_from_char(car, main_struct->map[local_pos], local_pos);
-	if (!compare_args(car->op_tabble.args, car->args))
+	read_args_from_char(car, (unsigned char)main_struct->map[local_pos],
+			local_pos);
+	local_pos++;
+	if (read_args(car, (unsigned char*)main_struct->map + local_pos))
 		return FALSE;
+	return TRUE;
 }
