e58cd2351697bb4bffd312589e176f397a778638
dburtnja <dostup.te@gmail.com>
2017-06-26 22:59:42 +0300

    add color when put_int_on_map

--- virtual_mashine/sti_operation.c
+++ virtual_mashine/sti_operation.c
@@ -15,10 +15,13 @@ void get_sti_func(t_car *car)
 
 void    do_sti_func(t_vm *main_struct, t_car *car)
 {
+	int map_pos;
+
+	map_pos = car->pos + (car->args[1].value + car->args[2].value) % MEM_SIZE;
 	if (car->args[0].name == T_REG && car->args[0].value > 0)
 	{
-		put_int_on_map(main_struct->map+car->pos + (car->args[1].value +
-							   car->args[2].value) % MEM_SIZE,car->reg[car->args[0].value - 1], main_struct->color,car->color);
+		put_int_on_map(main_struct->map + map_pos, car->reg[car->args[0].value - 1],
+					   main_struct->color + map_pos, car->color);
 	}
 	else
 	{
