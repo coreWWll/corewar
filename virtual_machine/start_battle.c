8b98121f9cbbc3869b264bb57affa9a5ec0b6dd0
yyefimov <nrpHefOj>
2017-07-02 12:52:22 +0300

    Merge branch 'master' of https://github.com/coreWWll/corewar
    # Conflicts:
    #	virtual_machine/bonus/check_if_refresh.c
    #	virtual_machine/bonus/ft_kbhit.c
    #	virtual_machine/bonus/put_caret_on_map.c
    #	virtual_machine/bonus/time.c
    #	virtual_machine/bonus/visualisate.c
    #	virtual_machine/bonus/visualisation.h
    #	virtual_machine/bonus/w_print_usage.c
    #	virtual_machine/get_args_and_values_part_2.c
    #	virtual_machine/vm.h

--- virtual_machine/start_battle.c
+++ virtual_machine/start_battle.c
@@ -10,6 +10,7 @@ void change_alive_flag(t_vm *main_struct)
 	t_player	**player;
 	t_car		*car;
 	int			i;
+
 	i = 0;
 	player = main_struct->players;
 	while (player[i])
@@ -18,13 +19,18 @@ void change_alive_flag(t_vm *main_struct)
 		player[i]->lives_in_current_period = 0;
 		while (car)
 		{
-			if (car->live == 0)
+			if (car->live == 0 || main_struct->cycle_to_die_for_viz <= 0)
 				dell_car_from_list(&(player[i]->car), car, main_struct);
 			car->live = 0;
 			car = car->next;
 		}
 		i++;
 	}
+	/*if (main_struct->cycle_to_die_for_viz <= 0)
+	{
+		main_struct->cycle_to_die_for_viz += CYCLE_DELTA;
+		main_struct->processes = 0;
+	}*/
 }
 
 int check_alive(t_vm *main_struct)
@@ -40,7 +46,7 @@ int check_alive(t_vm *main_struct)
 			return (1);
 		i++;
 	}
-    return (0);
+	return (0);
 }
 
 void	cycles_and_rounds(t_vm *main_struct)
@@ -52,7 +58,7 @@ void	cycles_and_rounds(t_vm *main_struct)
 		main_struct->cycle_to_die_for_viz = CYCLE_TO_DIE - CYCLE_DELTA *
 														   main_struct->round;
 		main_struct->cycle_to_die =	main_struct->cycle_to_die +
-				main_struct->cycle_to_die_for_viz;
+									   main_struct->cycle_to_die_for_viz;
 
 		main_struct->max_checks = 0;
 	}
@@ -73,14 +79,15 @@ void	the_winner_is(t_vm *main_struct)
 	if (main_struct->f_v == FALSE)
 		ft_printf("Player: %d -  \"%s\", has won.\n",
 				  -main_struct->players[main_struct->winner]->name,
-			  main_struct->players[main_struct->winner]->bot_name);
+				  main_struct->players[main_struct->winner]->bot_name);
 }
 
 void    start_battle(t_vm *main_struct)
 {
-		while (check_alive(main_struct) && (CYCLE_TO_DIE -
-			main_struct->round * CYCLE_DELTA) >= 0)
-    {
+	while (check_alive(main_struct))
+	{
+		if (main_struct->cycle_to_die_for_viz <= 0)
+			break ;
 		if (main_struct->f_dump && main_struct->dump_cycle == main_struct->cycle)
 			dump_memory((unsigned char *) main_struct->map, MEM_SIZE);
 		move_all_car(main_struct);
@@ -88,10 +95,10 @@ void    start_battle(t_vm *main_struct)
 		put_caret_on_map(main_struct);
 		if (main_struct->f_v == TRUE && main_struct->vis->go_to_end == FALSE)
 			visualisate(main_struct);
-        if (main_struct->cycle == main_struct->cycle_to_die)
+		if (main_struct->cycle == main_struct->cycle_to_die)
 			cycles_and_rounds(main_struct);
 		(main_struct->cycle)++;
-    }
+	}
 	if (main_struct->f_v == TRUE)
 	{
 		throw_visualization(main_struct, "BATTLE IS OVER");
@@ -99,4 +106,5 @@ void    start_battle(t_vm *main_struct)
 		stop_visualisation(main_struct);
 	}
 	the_winner_is(main_struct);
+
 }
