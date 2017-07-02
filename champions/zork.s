bb4a8ff7c1b937025aa9f40c4022c31c3abc9a07
arepnovs <repnovant@gmail.com>
2017-07-02 15:25:20 +0300

    + okres part

--- champions/zork.s
+++ champions/zork.s
@@ -1,8 +1,9 @@
 .name "zork"
-.comment "I'M ALIIIIVE"
+.comment "I'M ALIIIIVE"#dfhadfh
 
-l2:		sti r1, %:live, %1
+l2:		sti r1, %:live, %1#adfjldf
 		and r1, %0, r1
 
-live:	live %1
-		zjmp %:live
+live:	 #snsfhdfhs
+live %1
+		zjmp %:live #dfhadfh
